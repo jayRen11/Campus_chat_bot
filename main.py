333e0e8eaa328e7b208ef10a94cc47fe30604369
ren jay <renhaojie0102@gmail.com>
2026-05-02 15:36:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import sys
 
-__import__('pysqlite3')
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
 
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 
 from datetime import datetime
